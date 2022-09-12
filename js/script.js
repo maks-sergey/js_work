0c7269643f1580ba2495fac1cecb54dd0053f9b9
Maximenko <maks.sergey.11@gmail.com>
2022-09-12 10:16:56 +0300

    +modal

--- js/script.js
+++ js/script.js
@@ -41,25 +41,34 @@ const deadLine = '2022-09-15';
 
 function getTimeRemaining(endtime) {
     let days, hours, minutes, seconds;
-    const t = Date.parse(endtime) - Date.parse(new Date());
+    let t = Date.parse(endtime) - Date.parse(new Date());
+
         if (t <= 0) {
             days = 0;
             hours = 0;
             minutes = 0;
             seconds = 0;
+            return {
+                'total': t,
+                'days': days,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+                };
         } else {
-            days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-            minutes = Math.floor((t / 1000 / 60) % 60),
-            seconds = Math.floor((t / 1000) % 60);
-        }
+            days = Math.floor( (t / (1000 * 60 * 60 * 24)) );
+            seconds = Math.floor( (t / 1000) % 60 );
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+            minutes = Math.floor((t / 1000 / 60) % 60 );
+        
     return {
         'total': t,
         'days': days,
         'hours': hours,
         'minutes': minutes,
         'seconds': seconds
-    };
+        };
+    }
 }
 
         function getZero(num) {
@@ -79,24 +88,62 @@ function getTimeRemaining(endtime) {
                 timeInterval = setInterval(updateClock, 1000);
 
         updateClock();
-
+    
         function updateClock() {
             const t = getTimeRemaining(endtime);
+            console.log(t.total);
+            console.log(t);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
 
             days.innerHTML = getZero(t.days);
             hours.innerHTML = getZero(t.hours);
             minutes.innerHTML = getZero(t.minutes);
             seconds.innerHTML = getZero(t.seconds);
 
-            if (t.total <= 0) {
-                clearInterval(timeInterval);
-            }
+            
         }
     }
 
     setClock('.timer', deadLine);
 
-});
+    // Modal
 
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+            modal = document.querySelector('.modal'),
+            modalCloseBtn = document.querySelector('[data-close]');
 
     
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.toggle('show');
+            modal.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    
+    function closeModal () {
+        modal.classList.toggle('show');
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+
+        document.addEventListener('keydown', (e) => {
+            if (e.code === 'Escape' && modal.classList.contains('show')) {
+                closeModal();
+            }
+        });
+
+});
